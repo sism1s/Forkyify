fa17fbb6e7d91b88e91be3871550db99932bd1ac
sism1s <marko.lazic880@gmail.com>
2021-01-28 17:13:49 +0000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c0b85c99-114f-4e8c-9532-b3c0658fe1ca';
+export const MODAL_CLOSE_SEC = 2.5;
